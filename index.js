e862d4cdb909630c44c499f2a8143f38d5604ec6
bobbyhadz <opmftw@gmail.com>
2024-04-04 12:19:36 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.log('bobbyhadz.com');
 
 console.log(5 + 5);
+
+document.write(
+  `<div>
+    <h2>bobbyhadz.com</h2>
+
+    <p>
+     One, <span style="background-color: lime;">Two</span>, Three
+    </p>
+  </div>`,
+);
